22113fef81a25ed31cf2e610d39535e1a013b734
jetsadapudad <jetsadapd60@gmail.com>
2024-01-26 15:15:13 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+function main() {
+    let isSlash = false;
+    const eyeIcon = document.querySelector('#eye-icon');
+    const inputPassword = document.querySelector('#input-password');
+
+    eyeIcon.addEventListener('click', (el) => {
+        
+        if(!isSlash) {
+            eyeIcon.className = 'bi-eye-slash eye-icon';
+            inputPassword.setAttribute('type', 'text');
+        };
+        if(isSlash) {
+            eyeIcon.className = 'bi-eye eye-icon'
+            inputPassword.setAttribute('type', 'password');
+        };
+        isSlash = !isSlash;
+    });
+
+}
+
+main();
